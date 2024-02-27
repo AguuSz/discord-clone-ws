cd2e6501ad99c2cbe4d7c20217346ae48eb6a1c6
AguuSz <agus.sepu2000@hotmail.com>
2024-02-27 16:45:01 -0300

    first commit

---
+++ src/test/java/com/aguusz/discord/DiscordCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguusz.discord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
